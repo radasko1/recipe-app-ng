f5d07c59216d02f54dba470217d805189f1710d1
Radim Kolar <radimkolar22@gmail.com>
2024-02-14 21:23:06 +0100

    environment url

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -3,28 +3,18 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { environment } from '../../environments/environment';
-import { Ingredient } from '../models/ingredient.interface';
 import { Recipe } from '../models/recipe.interface';
 import { IngredientCategory } from '../models/ingredient-category.interface';
 
 @Injectable()
 export class DataService {
-  private readonly url = `${environment.recipeAppExpressUrl}/api/v1`;
-
-  constructor(private http: HttpClient) {}
-
-  /**
-   * Get list of all ingredients
-   */
-  getIngredients(): Observable<Ingredient[]> {
-    return this.http.get<Ingredient[]>(`${this.url}/ingredient`);
-  }
+  constructor(private readonly http: HttpClient) {}
 
   /**
    * Get list of all categories with ingredients
    */
   getCategories(): Observable<IngredientCategory[]> {
-    return this.http.get<IngredientCategory[]>(`${this.url}/ingredient/categories`);
+    return this.http.get<IngredientCategory[]>(`${environment.SERVER_API}/ingredient/categories`);
   }
 
   /**
@@ -32,7 +22,7 @@ export class DataService {
    * @param ingredientNames
    */
   findRecipes(ingredientNames: string[]): Observable<Recipe[]> {
-    return this.http.post<Recipe[]>(`${this.url}/recipe/ingredients`, {
+    return this.http.post<Recipe[]>(`${environment.SERVER_API}/recipe/ingredients`, {
       ingredients: ingredientNames,
     });
   }
