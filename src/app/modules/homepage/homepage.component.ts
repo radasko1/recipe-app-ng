29cbce96233af42aa11fa63b690c489fb204b9ff
Radim Kolar <radimkolar22@gmail.com>
2024-02-16 11:24:14 +0100

    move promo text on homepage

--- src/app/modules/homepage/homepage.component.ts
+++ src/app/modules/homepage/homepage.component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
+import locale from './locale.json';
+import { LanguageService } from '../../services/language.service';
 
 @Component({
   selector: 'app-homepage',
-  template: `<div>This is homepage</div>`,
+  template: `
+    <!--promo text-->
+    <div class="py-9 mx-14">
+      <h2 class="text-4xl font-semibold mb-1">{{ locale[langService.language].PromoText1 }}</h2>
+      <h2 class="text-4xl font-semibold">{{ locale[langService.language].PromoText2 }}</h2>
+    </div>
+    <!--  TODO may be part of Homepage with button to route into /search  -->
+  `,
 })
-export class HomepageComponent {}
+export class HomepageComponent {
+  protected readonly locale = locale;
+
+  constructor(protected readonly langService: LanguageService) {}
+}
--- src/app/modules/search/components/search/search.component.ts
+++ src/app/modules/search/components/search/search.component.ts
@@ -8,13 +8,6 @@ import { Ingredient } from '../../models/ingredient.interface';
 @Component({
   selector: 'app-search',
   template: `
-    <!--promo text-->
-    <!--  TODO may be part of Homepage with button to route into /search  -->
-    <div class="py-9 mx-14">
-      <h2 class="text-4xl font-semibold mb-1">{{ locale[langService.language].PromoText1 }}</h2>
-      <h2 class="text-4xl font-semibold">{{ locale[langService.language].PromoText2 }}</h2>
-    </div>
-
     <!--Search bar-->
     <div class="py-8 mx-14">
       <app-search-bar (onSubmit)="submit($event)" />
@@ -29,10 +22,7 @@ export class SearchComponent {
   protected readonly locale = locale;
   protected recipeList: Recipe[] = [];
 
-  constructor(
-    protected readonly langService: LanguageService,
-    private readonly recipeService: RecipeService
-  ) {}
+  constructor(private readonly recipeService: RecipeService) {}
 
   /**
    * Submit selected ingredients
