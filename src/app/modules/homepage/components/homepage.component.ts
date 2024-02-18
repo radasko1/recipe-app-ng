c741f2eae0405c6c11cddc5a0b12b81ed0893f28
Radim Kolar <radimkolar22@gmail.com>
2024-02-18 17:33:57 +0100

    button in homepage

--- src/app/modules/homepage/components/homepage.component.ts
+++ src/app/modules/homepage/components/homepage.component.ts
@@ -9,8 +9,16 @@ import { LanguageService } from '../../../services/language.service';
     <div class="py-9 mx-14">
       <h2 class="text-4xl font-semibold mb-1">{{ locale[langService.language].PromoText1 }}</h2>
       <h2 class="text-4xl font-semibold">{{ locale[langService.language].PromoText2 }}</h2>
+      <div class="mt-3">
+        <button
+          type="button"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          routerLink="/search"
+        >
+          {{ locale[langService.language].Button }}
+        </button>
+      </div>
     </div>
-    <!--  TODO may be part of Homepage with button to route into /search  -->
   `,
 })
 export class HomepageComponent {
