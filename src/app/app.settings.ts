8970c0a7a26e043b5e411a2c820657e57d7e7554
Radim Kolar <radimkolar22@gmail.com>
2024-02-29 14:50:56 +0100

    CSP headers?

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,3 +1,5 @@
+import { InjectionToken } from '@angular/core';
+
 const APPLICATION_NAME = 'RECIPO-MAT';
 
 const HOME_PAGE_TITLE = {
@@ -14,5 +16,11 @@ export const APP_PAGE_TITLE = {
   SEARCH: SEARCH_PAGE_TITLE,
 };
 
-
 // TODO add meta tags
+
+const ContentSecurityPolicy =
+  "default-src 'self'; style-src 'self' 'nonce-randomNonceGoesHere'; script-src 'self' 'nonce-randomNonceGoesHere';";
+export const CONTENT_SECURITY_POLICY_TOKEN = new InjectionToken<string>(
+  'ContentSecurityPolicyHeaders',
+  { providedIn: 'root', factory: () => ContentSecurityPolicy }
+);
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { enableProdMode } from '@angular/core';
+import { CSP_NONCE, enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { CONTENT_SECURITY_POLICY_TOKEN } from './app/app.settings';
 import { environment } from './environments/environment';
 
 if (environment.production) {
@@ -9,5 +10,12 @@ if (environment.production) {
 }
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+  .bootstrapModule(AppModule, {
+    providers: [
+      {
+        provide: CSP_NONCE,
+        useValue: CONTENT_SECURITY_POLICY_TOKEN,
+      },
+    ],
+  })
   .catch((err) => console.error(err));
