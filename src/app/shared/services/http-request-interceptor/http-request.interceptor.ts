392ec500e4eaec7558435ff1e96580075b3148c6
Radim Kolar <radimkolar22@gmail.com>
2026-06-07 12:55:28 +0200

    login page

--- src/app/shared/services/http-request-interceptor/http-request.interceptor.ts
+++ src/app/shared/services/http-request-interceptor/http-request.interceptor.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class HttpRequestInterceptor implements HttpInterceptor {
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // const headers = req.headers.set('Content-Type', 'application/json');
-    // const authReq = req.clone({ headers });
+  constructor(private readonly router: Router) {}
 
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const authReq = req.clone({ withCredentials: true });
-    return next.handle(authReq);
+    return next.handle(authReq).pipe(
+      catchError((error: unknown) => {
+        if (error instanceof HttpErrorResponse && [401, 403].includes(error.status)) {
+          this.router.navigateByUrl('/');
+        }
+
+        return throwError(() => error);
+      })
+    );
   }
 }
